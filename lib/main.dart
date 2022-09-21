255bc236f8bfbea1edc05a7d77ef030d6e61eb87
josungwoo <cso3297@gmail.com>
2022-09-21 20:14:29 +0900

    make tourlist

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,77 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp()); // runapp 안에서 메인 페이지를 구동시켜 준다
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  //여기부터
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    //여기까지는 기본적인 세팅
-
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('조성우'),
-        ),
-        body: Container(
-          margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-          padding: EdgeInsets.all(10),
-          color: Colors.grey,
-        ),
-        bottomNavigationBar: SizedBox(
-          height: 70,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
+          appBar: AppBar(
+            title: Text('관광지 리스트'),
+          ),
+          body: ListView.builder(itemBuilder: (context, int index) {
+            return Container(
+              height: 70,
+              color: Colors.blue[index * 200],
+              alignment: Alignment.center,
+              child: Text('data'),
+            );
+          }),
+      bottomNavigationBar: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Icon(Icons.phone),
-              Icon(Icons.message),
-              Icon(Icons.people)
+              Icon(Icons.list),
+              Text('리스트')
             ],
           ),
-        ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Icon(Icons.map),
+              Text('지도')
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Icon(Icons.sms),
+              Text('커뮤니티')
+
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Icon(Icons.memory),
+              Text('AI')
+
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Icon(Icons.perm_identity),
+              Text('프로필')
+
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Icon(Icons.settings),
+              Text('설정')
+            ],
+          ),
+        ],
+      ),
       ),
     );
   }
