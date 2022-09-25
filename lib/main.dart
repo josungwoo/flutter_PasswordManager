0fd4b04e862e967931ea009e6943d18d66d6ff27
josungwoo <cso3297@gmail.com>
2022-09-25 22:42:55 +0900

    clean bottom

--- lib/main.dart
+++ lib/main.dart
@@ -14,64 +14,8 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: Text('관광지 리스트'),
           ),
-          body: ListView.builder(itemBuilder: (context, int index) {
-            return Container(
-              height: 70,
-              color: Colors.blue[index * 200],
-              alignment: Alignment.center,
-              child: Text('data'),
-            );
-          }),
-      bottomNavigationBar: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Icon(Icons.list),
-              Text('리스트')
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Icon(Icons.map),
-              Text('지도')
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Icon(Icons.sms),
-              Text('커뮤니티')
+          body: Center(),
 
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Icon(Icons.memory),
-              Text('AI')
-
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Icon(Icons.perm_identity),
-              Text('프로필')
-
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Icon(Icons.settings),
-              Text('설정')
-            ],
-          ),
-        ],
-      ),
       ),
     );
   }
