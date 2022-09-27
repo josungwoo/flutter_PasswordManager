a89dd6931bb87628eab5f37ba0c1ea6405ad6070
josungwoo <cso3297@gmail.com>
2022-09-27 15:17:20 +0900

    make char page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 void main() {
-  runApp(new MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,12 +11,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('관광지 리스트'),
-          ),
-          body: Center(),
+      debugShowCheckedModeBanner: false,
+      title: "Character card", // 앱의 이름
+      theme: ThemeData(
+        primarySwatch: Colors.indigo
+      ),
+      home: Grade(),
+    );
+  }
+}
 
+
+class Grade extends StatelessWidget {
+  const Grade({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo[500],
+      appBar: AppBar(
+        title: Text("BBANTO"),
+        centerTitle: true,
+        backgroundColor: Colors.indigo[400],
+        elevation: 0,
+      ),
+      body:
+      Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('icon1.png'),
+                radius: 40,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
